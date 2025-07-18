286038362576b8020b5df7f249963c49ca5773ea
Chowa <arfinchowa524@gmail.com>
2025-07-18 23:40:53 +0600

    Six ways to do Conditional Rendering: If, Ternary and AND OR

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
+import { Todo } from "./Todo";
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <Person/>
+      <Person />
+      <Todo task="learn react " isDone={true} />
+      <Todo task="core concepts" isDone={false} />
+      <Todo task="try JSX" isDone={false} />
     </>
   );
 }
 
-
-
-function Person(){
-  return (
-  <h3>I am a person</h3>
-)
+function Person() {
+  return <h3>I am a person</h3>;
 }
 export default App;
---
+++ src/Todo.jsx
@@ -0,0 +1,48 @@
+
+// export default function Todo({ task, isDone }) {
+//   return (
+    
+//       <li>
+//        Task:{task}
+//       </li>
+   
+//   );
+// }
+
+
+// conditional rendaring
+
+// export default function Todo({ task, isDone }) {
+//   if (isDone ) {
+//     return <li>Finished:{task}</li>;
+//   }
+//     return <li>Work on: {task}</li>;
+
+// }
+
+// conditional rendaring 3:ternary operator
+
+// export default function Todo({ task, isDone }) {
+//   return (
+//     <ol>
+//       <li>
+//         {isDone ? "Finished" : "work on"} :{task}
+//       </li>
+//     </ol>
+//   );
+// }
+
+
+// conditional rendaring 4:&& ||
+// export default function Todo({ task, isDone }) {
+//   return (
+//     <ol>
+//       <li>
+//         {task}{isDone && ':Done'} 
+//       </li>
+//       <li>
+//         {task}{isDone || ':Do it'} 
+//       </li>
+//     </ol>
+//   );
+// }
