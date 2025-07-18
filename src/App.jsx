792d5c32cca78d0e696565282852d323adc70ed3
Chowa <arfinchowa524@gmail.com>
2025-07-18 23:11:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Person/>
+    </>
+  );
+}
+
+
+
+function Person(){
+  return (
+  <h3>I am a person</h3>
+)
+}
+export default App;
