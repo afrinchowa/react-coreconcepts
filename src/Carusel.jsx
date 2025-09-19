25edaa5d21e979e9f49c994e8ce254093ef92e92
Chowa <arfinchowa524@gmail.com>
2025-09-19 23:43:01 +0600

    caruselbanner added

---
+++ src/Carusel.jsx
@@ -0,0 +1,25 @@
+<CarouselBanner
+  slides={[
+    {
+      image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+      title: "Welcome to Paradise 🌴",
+      subtitle: "Discover amazing destinations around the world",
+      buttonText: "Explore Now",
+      onClick: () => alert("Explore clicked!"),
+    },
+    {
+      image: "https://images.unsplash.com/photo-1503264116251-35a269479413",
+      title: "Learn Web Development",
+      subtitle: "Modern tools, best practices, real projects",
+      buttonText: "Start Learning",
+      onClick: () => alert("Learning clicked!"),
+    },
+    {
+      image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+      title: "Build With React ⚛️",
+      subtitle: "Reusable components & scalable apps",
+      buttonText: "Get Started",
+      onClick: () => alert("React clicked!"),
+    },
+  ]}
+/>
